f030d11b94bf788285af2648a37ae1b504deabdb
marwenv1 <marwen.tlili@esprit.tn>
2021-01-21 01:09:06 +0100

    frontend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from "./pages/Home";
+import Helmet from "react-helmet";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <Helmet>
+          <style>
+            {
+              "body, html{ background-color: #F5F5F5; max-width: 100%; overflow-x: hidden}"
+            }
+          </style>
+        </Helmet>
+        <Home />
+      </div>
   );
 }
 
